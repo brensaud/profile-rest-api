60be0162f7daead30057500733c04ad2e6012c55
Biren Saud <brensaud4@gmail.com>
2021-06-22 19:11:40 +0545

    UserProfileViewSet created and profile router registered

--- app/profiles_api/views.py
+++ app/profiles_api/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework import viewsets
 
 from profiles_api import serializers
-
+from profiles_api import models
 
 class HelloAPIView(APIView):
     """Test API View"""
@@ -117,3 +117,9 @@ class HelloViewSet(viewsets.ViewSet):
             'http_method': 'DELETE'
         })
 
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
