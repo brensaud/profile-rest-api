d03aef3185d4f6eb866abb3a55bbd8a00662c7be
Biren Saud <brensaud4@gmail.com>
2021-06-22 15:40:47 +0545

    Created profiles rest_api app

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
-    'profiles',
+    'rest_framework'
+    'rest_framework.authtoken',
+    'profiles_api',
 ]
 
 MIDDLEWARE = [
@@ -120,5 +121,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-AUTH_USER_MODEL = 'core.User'
